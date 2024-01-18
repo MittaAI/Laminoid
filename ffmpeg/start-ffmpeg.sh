5ae438b0fef14e011b36b5de5ae701667ffe44c6
Kord Campbell <kordless@gmail.com>
2024-01-18 04:22:22 -0600

    ffmpeg

--- ffmpeg/start-ffmpeg.sh
+++ ffmpeg/start-ffmpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    screen -dmS gunicorn_screen gunicorn -b 0.0.0.0:6969 ffmpeg:app -w 2 --timeout 120
+    screen -dmS gunicorn_ffmpeg gunicorn -b 0.0.0.0:6969 ffmpeg:app -w 2 --timeout 120
     sleep 1
     
     while screen -list | grep -q gunicorn_ffmpeg; do
